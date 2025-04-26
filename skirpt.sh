5c2bc6282b4c2d6021449f347db1fd6ff22b885c
pomnysboinik <130384502+pomnysboinik@users.noreply.github.com>
2025-04-26 12:20:10 +0300

    Update skirpt.sh

--- skirpt.sh
+++ skirpt.sh
@@ -1,6 +1,30 @@
+#DevOps
+#!/bin/bash
+echo "Updating the system..."
+sudo apt update && sudo apt upgrade -y
+echo "Installing Docker..."
+sudo apt install docker.io -y
+sudo systemctl enable --now docker
+echo "Installing Docker Compose..."
+sudo apt install docker-compose -y
+echo "Installing Terraform..."
+sudo apt install unzip -y
 wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+echo "Installing Git..."
+sudo apt install git -y
+echo "Installing GitHub CLI (gh)..."
+sudo apt install gh -y
+echo "Installing Nginx..."
+sudo apt install nginx -y
+echo "Installing AWS CLI v2..."
+sudo git clone https://github.com/pomnysboinik/top.git
+echo "Checking installed versions..."
+docker --version
+docker-compose --version
+terraform --version
+git --version
+gh --version
+nginx -v
+aws --version
